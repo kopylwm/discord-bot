dc34fbf028c7292121ee621d096e74a0efc7b078
matvey <matvey@git.com>
2022-11-15 20:38:26 +0200

    Added log handling

---
+++ main.py
@@ -0,0 +1,9 @@
+import discord
+import logging
+
+# Write the logs to a file <discord.log>
+logger = logging.getLogger('discord')
+logger.setLevel(logging.DEBUG)
+handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
+handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
+logger.addHandler(handler)
