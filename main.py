41f9696935e3bbebf7ebcfdb677b966abd9aae58
matvey <matvey@git.com>
2022-11-17 18:56:12 +0200

    Changed some comments

--- main.py
+++ main.py
@@ -3,13 +3,13 @@ import logging
 import dotenv
 import os
 
-# Write the logs to a file <discord.log>
+# Write the logs to a <discord.log> file
 logger = logging.getLogger('discord')
 logger.setLevel(logging.DEBUG)
 handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
 handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
 logger.addHandler(handler)
 
-# Retrieve token from <.env>
+# Retrieve token from <.env> file
 dotenv.load_dotenv()
 TOKEN = str(os.getenv('TOKEN'))
