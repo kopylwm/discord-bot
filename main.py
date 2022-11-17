be5bc5ad32116de4418bd6602eb275fa9e258cd1
matvey <matvey@git.com>
2022-11-17 18:50:17 +0200

    Added retrieving of token

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import discord
 import logging
+import dotenv
+import os
 
 # Write the logs to a file <discord.log>
 logger = logging.getLogger('discord')
@@ -7,3 +9,7 @@ logger.setLevel(logging.DEBUG)
 handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
 handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
 logger.addHandler(handler)
+
+# Retrieve token from <.env>
+dotenv.load_dotenv()
+TOKEN = str(os.getenv('TOKEN'))
