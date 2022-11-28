02bd4dfb1697322bd43b5b645e7791221170f46f
matvey <matvey@git.com>
2022-11-28 12:28:47 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,38 @@
+# Pycord libraries
 import discord
 import logging
+from discord.ext import commands
+
+# Other libraries
 import dotenv
 import os
 
-# Write the logs to a <discord.log> file
-logger = logging.getLogger('discord')
-logger.setLevel(logging.DEBUG)
-handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
-handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
-logger.addHandler(handler)
+def main():
+    # Write logs to a <discord.log> file
+    logger = logging.getLogger('discord')
+    logger.setLevel(logging.DEBUG)
+    handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
+    handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
+    logger.addHandler(handler)
 
-# Retrieve token from <.env> file
-dotenv.load_dotenv()
-TOKEN = str(os.getenv('TOKEN'))
+    # Retrieve token from <.env> file
+    dotenv.load_dotenv()
+    TOKEN = os.getenv('TOKEN')
+
+    # Enable necessary intents
+    intents = discord.Intents.default()
+    intents.message_content = True
+
+    bot = commands.Bot(command_prefix='p?', intents=intents)
+
+    @bot.event
+    async def on_ready():
+        print(f'{bot.user} is ready!')
+
+    # Add all commands
+    bot.load_extension('cogs.music')
+    
+    bot.run(TOKEN)
+
+if __name__ == '__main__':
+    main()
