985869557a1f5eac9ebc5a6964e34217692c5d2a
matvey <matvey@git.com>
2022-11-29 16:18:04 +0200

    Changed command prefix

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ def main():
     intents = discord.Intents.default()
     intents.message_content = True
 
-    bot = commands.Bot(command_prefix='p?', intents=intents)
+    bot = commands.Bot(command_prefix='?', intents=intents)
 
     @bot.event
     async def on_ready():
