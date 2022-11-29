b80371469cbf7aa5d733e2fd9742dad218ce1db0
matvey <matvey@git.com>
2022-11-29 16:18:40 +0200

    Update music.py

--- cogs/music.py
+++ cogs/music.py
@@ -9,8 +9,12 @@ class Music(commands.Cog):
         self.bot = bot
 
         # Queue stuff
-        self.queue = wavelink.Queue()
+        self.queue: wavelink.Queue = wavelink.Queue()
         self.queue_ctx: commands.Context = None
+
+        # Loop stuff
+        self.loop_var: bool = False
+        self.loop_track: wavelink.YouTubeTrack = None
 
         bot.loop.create_task(self.connect_nodes())
 
@@ -30,13 +34,21 @@ class Music(commands.Cog):
 
     @commands.Cog.listener()
     async def on_wavelink_track_end(self, player: wavelink.Player, track: wavelink.YouTubeTrack, reason):
+        if self.loop_var:
+            await self.queue_ctx.send(f'**Now playing: {loop_track.title} [{datetime.timedelta(seconds=loop_track.length)}]**')
+            return await player.play(self.loop_track)
+
         if not self.queue:
             self.queue_ctx = None
             return
-        
-        track = self.queue.pop()
+
+        track = self.queue.get()
+
+        print('popped:', track.title)
+
         await self.queue_ctx.send(f'**Now playing: {track.title} [{datetime.timedelta(seconds=track.length)}]**')
         await player.play(track)
+        print('i should play music now:', track.title)
 
     @commands.command()
     async def connect(self, ctx: commands.Context):
@@ -63,7 +75,7 @@ class Music(commands.Cog):
 
     @commands.command()
     async def play(self, ctx: commands.Context, *, query: str):
-        """Play YT track with given query"""
+        """Add YT track to the queue and play it"""
         await ctx.invoke(self.connect)
         vc = ctx.voice_client
 
@@ -76,8 +88,11 @@ class Music(commands.Cog):
         if not track:
             return await ctx.send('**No track found with given query.**')
 
-        self.queue.put(track)
         await ctx.send(f'**Added to the queue: {track.title} [{datetime.timedelta(seconds=track.length)}]**')
+        self.queue.put(track)
+        
+        if self.loop_var and self.loop_track is None:
+            self.loop_track = self.queue.get()
 
         self.queue_ctx = ctx
         if not vc.is_playing():
@@ -85,36 +100,61 @@ class Music(commands.Cog):
 
     @commands.command()
     async def skip(self, ctx: commands.Context):
-        """Skips track to the next one"""
-        vc: wavelink.Player = ctx.voice_client
-
-        if not vc.is_playing:
-            return
+        """Skip current track to the next one"""
+        try:
+            vc: wavelink.Player = ctx.voice_client
+            if not vc.is_playing():
+                return
+            if ctx.author.voice.channel.id != vc.channel.id:
+                return await ctx.send('**You must be in the same voice channel as the player.**')
+        except: return
         
+        await ctx.send(f'**Skipped {vc.track.title} [{datetime.timedelta(seconds=vc.track.length)}]**')
         await vc.stop()
-        await ctx.send(f'**Skipped {self.queue[0].title} [{datetime.timedelta(seconds=self.queue[0].length)}]**')
-        await self.on_wavelink_track_end(player=vc, track=self.queue[0], reason=None)
+        
+        if self.loop:
+            self.loop_track = self.queue.get()
+
+    @commands.command()
+    async def clear(self, ctx: commands.Context):
+        """Stop current track and clear the queue"""
+        try:
+            vc: wavelink.Player = ctx.voice_client
+            if ctx.author.voice.channel.id != vc.channel.id:
+                return await ctx.send('**You must be in the same voice channel as the player.**')
+        except: return
+
+        if vc.is_playing():
+            await vc.stop()
+        if self.queue:
+            await self.queue.clear()
+
+        await ctx.send(f'**Stopped current track and cleared the queue**')
 
     @commands.command()
     async def queue(self, ctx: commands.Context):
-        """Lists all tracks in the queue"""
+        """List all tracks in the queue"""
         if not self.queue:
             await ctx.send('**The queue is empty**')
         for idx, track in enumerate(self.queue):
             await ctx.send(f'{idx + 1}. {track.title} [{datetime.timedelta(seconds=track.length)}]')
 
     @commands.command()
-    async def stop(self, ctx: commands.Context):
-        """Stops track and clears the queue"""
-        vc: wavelink.Player = ctx.voice_client
+    async def loop(self, ctx: commands.Context):
+        """Enable/Disable track loop"""
+        if self.loop_var:
+            self.loop_var = False
+            self.loop_track = None
+            return await ctx.send('**Disabled track loop**')
 
-        if not vc.is_playing():
-            return
+        try:
+            vc: wavelink.Player = ctx.voice_client
+            if vc.is_playing():
+                self.loop_track = vc.track
+        except: pass
 
-        await vc.stop()
-        await ctx.send(f'**Cleared the queue**')
-        await self.queue.clear()
-
+        self.loop_var = True
+        await ctx.send('**Enabled track loop**')
 
 def setup(bot: commands.Bot):
     bot.add_cog(Music(bot))
