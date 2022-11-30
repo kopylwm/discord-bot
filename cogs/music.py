63343e80df30e5553ead0bafb849e9d567b0eb44
matvey <matvey@git.com>
2022-11-30 14:35:15 +0200

    Update music.py

--- cogs/music.py
+++ cogs/music.py
@@ -1,5 +1,5 @@
-from discord.ext import commands
 import discord
+from discord.ext import commands
 
 import wavelink
 import datetime
@@ -32,7 +32,7 @@ class Music(commands.Cog):
     @commands.Cog.listener()
     async def on_wavelink_node_ready(self, node: wavelink.Node):
         # Fire event when a node has finished connecting
-        print(f'Node: <{node.identifier}> is ready!')
+        print(f'[lavalink]: node <{node.identifier}> is ready!')
 
     @commands.Cog.listener()
     async def on_wavelink_track_end(self, player: wavelink.Player, track: wavelink.YouTubeTrack, reason):
@@ -51,7 +51,7 @@ class Music(commands.Cog):
             track = self.queue.get()
         except: return
 
-        await self.queue_ctx.send(f'**Now playing: {track.title} [{datetime.timedelta(seconds=track.length)}]**')
+        await self.queue_ctx.send(embed=self._return_embed('PLAY_TRACK', track=track))
         await player.play(track)
 
     @commands.command()
@@ -66,7 +66,7 @@ class Music(commands.Cog):
             # Make bot deaf
             await ctx.guild.change_voice_state(channel=ctx.author.voice.channel, self_mute=False, self_deaf=True)
         except:
-            await ctx.send('**Cannot connect to the voice channel.**')
+            await ctx.send(embed=self._return_embed('CONN_FAIL'))
 
     @commands.command()
     async def disconnect(self, ctx: commands.Context):
@@ -85,20 +85,20 @@ class Music(commands.Cog):
 
         # Check if the player is in the same voice channel as the author
         if ctx.author.voice.channel.id != vc.channel.id:
-            return await ctx.send('**You must be in the same voice channel as the player.**')
+            return await ctx.send(embed=self._return_embed('NOT_SAME_VC'))
 
         track: wavelink.YouTubeTrack = await wavelink.YouTubeTrack.search(query=query, return_first=True)
 
         if not track:
-            return await ctx.send('**No track found with given query.**')
+            return await ctx.send(embed=self._return_embed('NO_TRACK'))
 
-        await ctx.send(f'**Added to the queue: {track.title} [{datetime.timedelta(seconds=track.length)}]**')
+        await ctx.send(embed=self._return_embed('ADD_TRACK', track=track))
         self.queue.put(track)
         self.saved_queue.put(track)
         
         if self.loop_var and self.loop_track is None:
             self.loop_track = self.queue.get()
-            await self.queue_ctx.send(f'**Now playing: {self.loop_track.title} [{datetime.timedelta(seconds=self.loop_track.length)}]**')
+            await ctx.send(embed=self._return_embed('PLAY_TRACK'))
 
         self.queue_ctx = ctx
         if not vc.is_playing():
@@ -110,50 +110,58 @@ class Music(commands.Cog):
         try:
             vc: wavelink.Player = ctx.voice_client
             if not vc.is_playing():
-                return
+                return await ctx.send(embed=self._return_embed('VC_NOT_PLAY'))
+
             if ctx.author.voice.channel.id != vc.channel.id:
-                return await ctx.send('**You must be in the same voice channel as the player.**')
+                return await ctx.send(embed=self._return_embed('NOT_SAME_VC'))
         except: return
         
-        await ctx.send(f'**Skipped {vc.track.title} [{datetime.timedelta(seconds=vc.track.length)}]**')
+        await ctx.send(embed=self._return_embed('SKIP'))
         await vc.stop()
         
         if self.loop_var:
             self.loop_track = self.queue.get()
-            await self.queue_ctx.send(f'**Now playing: {self.loop_track.title} [{datetime.timedelta(seconds=self.loop_track.length)}]**')
+            await ctx.send(embed=self._return_embed('PLAY_TRACK'))
 
     @commands.command()
-    async def clear(self, ctx: commands.Context):
+    async def stop(self, ctx: commands.Context):
         """Stop current track and clear the queue"""
         try:
             vc: wavelink.Player = ctx.voice_client
             if ctx.author.voice.channel.id != vc.channel.id:
-                return await ctx.send('**You must be in the same voice channel as the player.**')
+                return await ctx.send(embed=self._return_embed('NOT_SAME_VC'))
         except: return
 
+        self.loop_var = False
+
         if self.saved_queue:
-            await self.queue.clear()
-            await self.saved_queue.clear()
+            if self.queue:
+                self.queue.clear()
+            self.saved_queue.clear()
+
         if vc.is_playing():
             await vc.stop()
         
-        await ctx.send(f'**Stopped current track and cleared the queue**')
+        await ctx.send(embed=self._return_embed('STOP'))
+
+        self.loop_var = True if self.loop_track else False
+        self.loop_track = None
 
     @commands.command()
     async def queue(self, ctx: commands.Context):
         """List all tracks in the queue"""
         if not self.saved_queue:
-            await ctx.send('**The queue is empty**')
-        for idx, track in enumerate(self.saved_queue):
-            await ctx.send(f'{idx + 1}. {track.title} [{datetime.timedelta(seconds=track.length)}]')
+            return await ctx.send(embed=self._return_embed('QUEUE_EMPTY'))
+        await ctx.send(embed=self._return_embed('QUEUE'))
 
     @commands.command()
     async def loop(self, ctx: commands.Context):
         """Enable/Disable track loop"""
+        await ctx.send(embed=self._return_embed('LOOP'))
         if self.loop_var:
             self.loop_var = False
             self.loop_track = None
-            return await ctx.send('**Disabled track loop**')
+            return
 
         try:
             vc: wavelink.Player = ctx.voice_client
@@ -162,17 +170,134 @@ class Music(commands.Cog):
         except: pass
 
         self.loop_var = True
-        await ctx.send('**Enabled track loop**')
 
     @commands.command()
     async def loop_queue(self, ctx: commands.Context):
         """Enable/Disable queue loop"""
-        if self.queue_loop_var:
-            self.queue_loop_var = False
-            return await ctx.send('**Disabled queue loop**')
+        await ctx.send(embed=self._return_embed('QUEUE_LOOP'))
+        self.queue_loop_var = False if self.queue_loop_var else True
+    
+    def _return_embed(self, idx: str, track: wavelink.YouTubeTrack = None) -> discord.Embed:
+        """
+        Returns a discord.Embed object
 
-        self.queue_loop_var = True
-        await ctx.send('**Enabled queue loop**')
+        idx:
+        'VC_NOT_PLAY' - if the player is not playing anything
+        'NOT_SAME_VC' - if the player is in the same voice channel as the author
+        'CONN_FAIL' - if player cannot connect to the voice channel
+        'NO_TRACK' - if no track was found with given query
+        'PLAY_TRACK' - give info about playing track
+        'ADD_TRACK' - give info about added track to the queue
+        'SKIP' - skip current track
+        'STOP' - stop current track and clear the queue
+        'QUEUE_EMPTY' - if queue is empty
+        'QUEUE' - give info about queue
+        'LOOP' - enable/disable track looping
+        'QUEUE_LOOP' - enable/disable queue looping
+        """
+        if idx == 'VC_NOT_PLAY':
+            embed = discord.Embed(
+                title=None,
+                description=':x: **Nothing is playing right now!**',
+                color=discord.Color.dark_red()
+            )
+            return embed
+        elif idx == 'NOT_SAME_VC':
+            embed = discord.Embed(
+                title=None,
+                description=':x: **You must be in the same voice channel as the player!**',
+                color=discord.Color.dark_red()
+            )
+            return embed
+        elif idx == 'CONN_FAIL':
+            embed = discord.Embed(
+                title=None,
+                description=':x: **Cannot connect to the voice channel!**',
+                color=discord.Color.dark_red()
+            )
+            return embed
+        elif idx == 'NO_TRACK':
+            embed = discord.Embed(
+                title=None,
+                description=':x: **No track found with given query!**',
+                color=discord.Color.dark_red()
+            )
+            return embed
+        elif idx == 'PLAY_TRACK':
+            track = self.loop_track if track is None else track
+
+            embed = discord.Embed(
+                title=':anger: Now playing',
+                description=None,
+                color=discord.Color.dark_red()
+            )
+            embed.set_thumbnail(url=track.thumb)
+            embed.add_field(name='**Title**', value=track.title, inline=True)
+            embed.add_field(name='**Author**', value=track.author, inline=True)
+            embed.add_field(name='**Duration**', value=datetime.timedelta(seconds=track.duration), inline=True)
+            return embed
+        elif idx == 'ADD_TRACK':
+            embed = discord.Embed(
+                title=':anger: Added to the queue',
+                description=None,
+                color=discord.Color.dark_red()
+            )
+            embed.set_thumbnail(url=track.thumb)
+            embed.add_field(name='**Title**', value=track.title, inline=True)
+            embed.add_field(name='**Author**', value=track.author, inline=True)
+            embed.add_field(name='**Duration**', value=datetime.timedelta(seconds=track.duration), inline=True)
+            return embed
+        elif idx == 'SKIP':
+            vc: wavelink.Player = self.queue_ctx.voice_client
+
+            embed = discord.Embed(
+                title=':anger: Skipped current track',
+                description=None,
+                color=discord.Color.dark_red()
+            )
+            embed.set_thumbnail(url=vc.track.thumb)
+            embed.add_field(name='**Title**', value=vc.track.title, inline=True)
+            embed.add_field(name='**Author**', value=vc.track.author, inline=True)
+            embed.add_field(name='**Duration**', value=datetime.timedelta(seconds=vc.track.duration), inline=True)
+            return embed
+        elif idx == 'STOP':
+            embed = discord.Embed(
+                title=None,
+                description=':anger: **Stopped current track and cleared the queue!**',
+                color=discord.Color.dark_red()
+            )
+            return embed
+        elif idx == 'QUEUE_EMPTY':
+            embed = discord.Embed(
+                title=None,
+                description=':anger: **The queue is empty!**',
+                color=discord.Color.dark_red()
+            )
+            return embed
+        elif idx == 'QUEUE':
+            queue_list: str = '\n'.join([f'**{i + 1}. {song.title}**' for i, song in enumerate(self.saved_queue)])
+
+            embed = discord.Embed(
+                title=None,
+                description=queue_list,
+                color=discord.Color.dark_red()
+            )
+            return embed
+        elif idx == 'LOOP':
+            embed = discord.Embed(
+                title=None,
+                description=':anger: **Disabled track loop!**' if self.loop_var else ':anger: **Enabled track loop!**',
+                color=discord.Color.dark_red()
+            )
+            return embed
+        elif idx == 'QUEUE_LOOP':
+            embed = discord.Embed(
+                title=None,
+                description=':anger: **Disabled queue loop!**' if self.queue_loop_var else ':anger: **Enabled queue loop!**',
+                color=discord.Color.dark_red()
+            )
+            return embed
+        # Sadly match case statement is not working for me :(
 
 def setup(bot: commands.Bot):
     bot.add_cog(Music(bot))
